5b4a9b0ba00627e2331fd03e3d28d6d54d7514cc
Bruno Mikus <bikush.mruno@gmail.com>
2016-04-14 14:18:39 +0200

    Used jinja2 to generate newpost page. Placed URLs in globals to use in redirects.

--- blog.py
+++ blog.py
@@ -5,6 +5,13 @@ import os
 template_dir = os.path.join(os.path.dirname(__file__), 'assignment-3')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
+URL_MAIN = "/assignment-3/blog"
+URL_NEWPOST = "/assignment-3/blog/newpost"
+
+########################################
+# /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\ #
+########################################
+
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
@@ -14,18 +21,33 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw): 
         self.write(self.render_str(template, **kw))
 
+########################################
+# /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\ #
+########################################
 
-class NewPost(webapp2.RequestHandler):
+class NewPost(Handler):
+    def generate_newpost_page(self):
+        self.render("newpost.html", main_url=URL_MAIN)
+
     def get(self):
-        self.response.write("New post page.")
+        self.generate_newpost_page()
 
+    def post(self):
+        self.redirect(URL_MAIN)
 
-class MainPage(webapp2.RequestHandler):
+########################################
+# /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\ #
+########################################
+
+class MainPage(Handler):
     def get(self):
-        self.response.write("Main page. <a href=\"/assignment-3/blog/newpost\">New post</a>")
+        self.response.write("Main page. <a href=\""+URL_NEWPOST+"\">New post</a>")
 
+########################################
+# /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\ #
+########################################
 
 app = webapp2.WSGIApplication([
-    ('/assignment-3/blog/newpost', NewPost),
-    ('/assignment-3/blog', MainPage)
+    (URL_NEWPOST, NewPost),
+    (URL_MAIN, MainPage)
 ], debug=True)
