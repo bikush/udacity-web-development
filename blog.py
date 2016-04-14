d13e554895f0dc154ea495651c83f36d52871a82
Bruno Mikus <bikush.mruno@gmail.com>
2016-04-14 14:17:44 +0200

    Imported jinja2 in asignment-3.

--- blog.py
+++ blog.py
@@ -1,4 +1,18 @@
 import webapp2
+import jinja2
+import os
+
+template_dir = os.path.join(os.path.dirname(__file__), 'assignment-3')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+    def render(self, template, **kw): 
+        self.write(self.render_str(template, **kw))
 
 
 class NewPost(webapp2.RequestHandler):
